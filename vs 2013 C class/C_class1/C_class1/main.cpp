6e5a12faf3ba4327edc42ab7a87f96cd3b5cf8d4
jhg000 <112610280+jhg000@users.noreply.github.com>
2022-09-12 22:31:25 +0900

    Add files via upload

---
+++ vs 2013 C class/C_class1/C_class1/main.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+
+int main(void)
+{
+
+	/*
+	ずっさげっいすだ じたぁさっしぞたい C そぱぉでぁぱぉだけしびげいびぇた.
+	2022.09.12
+	
+	
+	
+	*/
+
+
+
+	printf("Hello  !  \n\n")
+	printf("I am a student. \n");
+	printf("%d\n",1234);
+	printf("%d   %d \n", 10, 20);
+	return 1;
+
+
+
+
+
+}
+
+
